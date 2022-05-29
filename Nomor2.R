d750402396125f119fe49312536bddb983fee64e
Sidrotul Munawaroh <94377420+SidrotulMunawaroh@users.noreply.github.com>
2022-05-29 02:10:46 -1200

    Create Nomor2.R

---
+++ Nomor2.R
@@ -0,0 +1,12 @@
+#2a. Apakah Anda setuju dengan klaim tersebut? Setujuuu
+#2b. Jelaskan maksud dari output yang dihasilkan!
+library(BSDA)
+
+   tsum.test(
+   mean.x = 23500, 
+   s.x = sd(3900), 
+   n.x = 100
+   )
+   
+#2c. Buatlah kesimpulan berdasarkan P-Value yang dihasilkan!
+kesimpulan dari soal diatas adalah mobil yang digunakan rata ratanya lebih dari 20.000 km/tahun
