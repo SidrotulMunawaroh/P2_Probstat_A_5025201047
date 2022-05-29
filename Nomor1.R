6461b76c065ed646236a622252b114cb2268a227
Sidrotul Munawaroh <94377420+SidrotulMunawaroh@users.noreply.github.com>
2022-05-28 22:02:29 -1200

    Update and rename Nomor1.r to Nomor1.R

--- Nomor1.R
+++ Nomor1.R
@@ -1,5 +1,4 @@
-1a. Carilah Standar Deviasi dari data selisih pasangan pengamatan tabel
-diatas
+#1a. Carilah Standar Deviasi dari data selisih pasangan pengamatan tabel diatas
 x = c(78,75,67,77,70,72,78,74,77)
 y = c(100,95,70,90,90,90,89,90,100)
 
