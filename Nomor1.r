7ecd0d54e9f059d9b366edbee48144160dd3b242
Sidrotul Munawaroh <94377420+SidrotulMunawaroh@users.noreply.github.com>
2022-05-28 22:00:55 -1200

    Create Nomor1.r

---
+++ Nomor1.r
@@ -0,0 +1,7 @@
+1a Carilah Standar Deviasi dari data selisih pasangan pengamatan tabel
+diatas
+x = c(78,75,67,77,70,72,78,74,77)
+y = c(100,95,70,90,90,90,89,90,100)
+
+data = data.frame(x,y)
+sd(data$x-data$y)
