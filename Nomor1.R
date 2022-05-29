67650d4d8a47393f1b63a4e968f603bb3c0be839
Sidrotul Munawaroh <94377420+SidrotulMunawaroh@users.noreply.github.com>
2022-05-29 02:07:28 -1200

    Update Nomor1.R

--- Nomor1.R
+++ Nomor1.R
@@ -4,3 +4,10 @@ y = c(100,95,70,90,90,90,89,90,100)
 
 data = data.frame(x,y)
 sd(data$x-data$y)
+
+#1b. carilah nilai t (p-value)
+t.test(x, y, alternative = "greater", var.equal = FALSE)
+
+#1c. tentukanlah apakah terdapat pengaruh yang signifikan secara statistika dalam hal kadar saturasi oksigen , sebelum dan sesudah melakukan aktivitas 𝐴 jika diketahui tingkat signifikansi 𝛼 = 5% serta H0 : “tidak ada pengaruh yang signifikan secara statistika dalam hal kadar saturasi oksigen , sebelum dan sesudah melakukan aktivitas 𝐴”
+var.test(x,y)
+t.tes(x,y, alternative = "two.sided", mu=0)
